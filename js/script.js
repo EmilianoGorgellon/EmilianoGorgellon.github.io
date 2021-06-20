5ad5cd82de77c5657d2313afcf8c911855d799fc
Emiliano Gorgellon <emilianogorgellon49@gmail.com>
2021-06-20 20:40:32 -0300

    Creacion del portafolio

---
+++ js/script.js
@@ -0,0 +1,94 @@
+const iconMenu = document.getElementById("icon-menu");
+const mainMenu = document.getElementById("main-menu");
+const articles = document.querySelectorAll('#proyectos article');
+const skills = document.querySelectorAll('#skills i');
+const mongo = document.getElementById("mongo");
+const titleSpan = document.getElementById("title-span");
+const headerTitle = document.getElementById("header-title");
+headerTitle.textContent = "< Emi "
+titleSpan.textContent = "<FRONT END DEVELOPER />";
+
+
+iconMenu.addEventListener('click', () => {
+    mainMenu.classList.toggle("menu--show")
+})
+const descriptionProyect = [
+    {"title":"App clima", "description":"Una app del clima","icon": ["fab fa-react", "fab fa-sass", "fab fa-html5"] , "id":"clima", "link": "fab fa-github", "page": "fas fa-globe-americas"}, 
+    {"title":"App Paises", "description": "Una app con muchos paises y con theme", "icon": ["fab fa-react", "fab fa-sass", "fab fa-html5"], "id": "paises", "link": "fab fa-github", "page": "fas fa-globe-americas"}, 
+    {"title":"Cuenta Regresiva", "description": "Cuenta regresiva de mi cumpleaños", "icon": ["fab fa-js-square", "fab fa-css3-alt", "fab fa-html5"], "id": "cuentaRegresiva", "link": "fab fa-github", "page": "fas fa-globe-americas"}, 
+    {"title":"Cuenta Regresiva", "description": "Cuenta regresiva de mi cumpleaños", "icon": ["fab fa-js-square", "fab fa-css3-alt", "fab fa-html5"], "id": "cuentaA", "link": "fab fa-github", "page": "fas fa-globe-americas"}
+]
+const textSkill = [
+    {"id":"html", "text":"HTML5"},
+    {"id":"css", "text":"Css3"}, 
+    {"id":"js", "text":"JavaScript"}, 
+    {"id":"react", "text": "React"}, 
+    {"id": "sass", "text": "Sass"},
+    {"id":"node", "text":"Node JS"}, 
+    {"id":"bootstrap", "text": "Bootstrap"},
+    {"id":"github", "text":"Github"},
+    {"id":"git", "text":"Git"},
+    {"id":"mongo", "text":"MongoDB"}
+]
+
+const rellenar = (e) => {
+    for (datos of descriptionProyect){
+        let iconos = datos.icon.map((dato) => `<i class="${dato} icon"></i>`);
+        let iconosSinComas = iconos.join("");
+        if (datos.id === e.target.id) {
+            document.getElementById(`${datos.id}`).insertAdjacentHTML('afterbegin', 
+                `<div class="container--information-proyect">
+                    <h3 class="information--title">${datos.title} </h3>
+                    <p class="information--description">${datos.description}</p>
+                    <div class="container-icon">
+                        ${iconosSinComas}
+                    </div>
+                    <div class="container--information-links">
+                       
+                        <a href="#" class="link-github"><i class="${datos.link}"></i> Github</a>
+                        <a href="#" class="link-page"><i class="${datos.page}"></i> Visitar sitio web</a>
+                    </div>
+                </div>`)            
+            break
+        }
+    }
+}
+
+const skillText = (e) => {
+    for (datos of textSkill){
+        if (e.target.id === datos.id){
+            document.getElementById(`${datos.id}`).insertAdjacentHTML('afterbegin', 
+                `<h5 class="text-skill">${datos.text}</h5>`
+            )
+        }
+    }
+}
+const skillMongo = (e) => {
+    for (datos of textSkill){
+        if (e.target.id === datos.id){
+            document.getElementById(`${datos.id}`).insertAdjacentHTML('beforeend', 
+                `<h5 class="text-skill-mongo" id="deleteMongo">${datos.text}</h5>`
+            )
+        }
+    }   
+}
+
+articles.forEach((article) => {
+    article.addEventListener('mouseenter', rellenar)
+    article.addEventListener('mouseleave', (e) => document.getElementById(`${e.target.id}`).innerHTML = "");
+
+})
+
+skills.forEach((skill) => {
+    skill.addEventListener('mouseenter', skillText)
+    skill.addEventListener('mouseleave', (e) => document.getElementById(`${e.target.id}`).innerHTML = "");
+})
+
+mongo.addEventListener('mouseenter', skillMongo)
+mongo.addEventListener('mouseleave', () => document.getElementById(`mongo`).removeChild(deleteMongo));
+
+
+ScrollReveal().reveal('.container--profile',);
+ScrollReveal().reveal('.container-proyects', {delay:300})
+ScrollReveal().reveal('.about-me-description', {delay:200})
+ScrollReveal().reveal('.about-me-skills', {delay:400})
